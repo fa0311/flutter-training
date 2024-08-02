44548410f8d40c89acb070223c053161ff1518e7
ふぁ <yuki@yuki0311.com>
2024-08-02 17:34:35 +0900

    rename: ファイルの名前を具体的なものに変更する

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/screen.dart';
+import 'package:flutter_training/view/weather.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
