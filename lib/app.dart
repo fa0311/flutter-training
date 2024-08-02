0f9f77d84e9459fbee1c70b432dba0af05c2c634
ふぁ <yuki@yuki0311.com>
2024-08-02 17:04:00 +0900

    update: ウィジェットを分割する

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/view/weather.dart';
+import 'package:flutter_training/screen.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -7,7 +7,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: WeatherScreen(),
+      home: WeatherApp(),
     );
   }
 }
