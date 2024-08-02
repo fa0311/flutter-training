eac2a4b389a43b6111f06541acb94c63cf6ebf79
ふぁ <yuki@yuki0311.com>
2024-08-02 16:25:18 +0900

    feat: reload ボタンの実装を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,60 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_training/component/action_buttons.dart';
 import 'package:flutter_training/component/temperature.dart';
+import 'package:flutter_training/domain/weather_service.dart';
+import 'package:flutter_training/model/weather_model.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final WeatherService weatherService = WeatherService();
+  WeatherType? weatherCondition;
+
+  void _reloadWeather() {
+    setState(() {
+      try {
+        weatherCondition = weatherService.fetch();
+        debugPrint(weatherCondition.toString());
+      } on Exception {
+        weatherCondition = null;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: FractionallySizedBox(
             widthFactor: 0.5,
             child: Column(
               children: [
-                Spacer(),
-                AspectRatio(
+                const Spacer(),
+                const AspectRatio(
                   aspectRatio: 1,
                   child: Placeholder(),
                 ),
-                Temperature(),
+                const Temperature(),
                 Flexible(
                   child: Padding(
-                    padding: EdgeInsets.only(top: 80),
-                    child: ActionButtons(),
+                    padding: const EdgeInsets.only(top: 80),
+                    child: ActionButtons(
+                      closePressed: () {
+                        throw UnimplementedError();
+                      },
+                      reloadPressed: _reloadWeather,
+                    ),
                   ),
                 ),
               ],
@@ -36,5 +63,15 @@ class MainApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      DiagnosticsProperty<WeatherService>('weatherService', weatherService),
+    );
+    properties
+        .add(EnumProperty<WeatherType?>('weatherCondition', weatherCondition));
   }
 }
