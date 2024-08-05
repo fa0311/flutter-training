11f1c79291e06cd1b765b3a912f901957ab37381
ふぁ <yuki@yuki0311.com>
2024-08-05 13:38:12 +0900

    fix: Padding の Typo を修正

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MainApp extends StatelessWidget {
                 ),
                 Flexible(
                   child: Padding(
-                    padding: EdgeInsets.only(top: 40),
+                    padding: EdgeInsets.only(top: 80),
                     child: ActionButton(),
                   ),
                 ),
