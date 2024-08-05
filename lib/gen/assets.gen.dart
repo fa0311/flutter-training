f3e56e3d9ce8e5640af8ef10426f66ea25fd5bdf
ふぁ <yuki@yuki0311.com>
2024-08-05 16:11:55 +0900

    feat: build_runner を実行

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsWeatherGen {
+  const $AssetsWeatherGen();
+
+  /// File path: assets/weather/cloudy.svg
+  String get cloudy => 'assets/weather/cloudy.svg';
+
+  /// File path: assets/weather/rainy.svg
+  String get rainy => 'assets/weather/rainy.svg';
+
+  /// File path: assets/weather/sunny.svg
+  String get sunny => 'assets/weather/sunny.svg';
+
+  /// List of all assets
+  List<String> get values => [cloudy, rainy, sunny];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsWeatherGen weather = $AssetsWeatherGen();
+}
