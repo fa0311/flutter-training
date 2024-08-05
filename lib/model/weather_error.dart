b7f962cfd2ff069f13b4e9695983d569b80d8693
ふぁ <yuki@yuki0311.com>
2024-08-05 17:04:21 +0900

    feat: weather service のエラーハンドリングを行う

---
+++ lib/model/weather_error.dart
@@ -0,0 +1,7 @@
+class WeatherExeption implements Exception {}
+
+class WeatherInvalidParameterExeption implements WeatherExeption {}
+
+class WeatherUnknownExeption implements WeatherExeption {}
+
+class WeatherInvalidResponseExeption implements WeatherExeption {}
