9275952bff6d7c197816444e23d728f381b8437a
ふぁ <yuki@yuki0311.com>
2024-08-08 16:31:53 +0900

    feat: エラーが発生したときのテストを追加

--- test/domain/weather_service_test.dart
+++ test/domain/weather_service_test.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_training/domain/weather_service.dart';
+import 'package:flutter_training/model/weather_error.dart';
 import 'package:flutter_training/model/weather_model.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -21,5 +22,29 @@ void main() {
     when(mock.fetchWeather(any)).thenReturn(jsonString);
     final param = WeatherParameterModel(area: 'tokyo', date: DateTime.now());
     final _ = service.fetch(param);
+  });
+
+  test('fetch でAPIに invalidParameter エラーが発生したとき', () {
+    final mock = MockYumemiWeather();
+    final service = WeatherService(client: mock);
+    final param = WeatherParameterModel(area: 'tokyo', date: DateTime.now());
+
+    when(mock.fetchWeather(any)).thenThrow(YumemiWeatherError.invalidParameter);
+    expect(
+      () => service.fetch(param),
+      throwsA(const TypeMatcher<WeatherInvalidParameterException>()),
+    );
+  });
+
+  test('fetch でAPIに unknown エラーが発生したとき', () {
+    final mock = MockYumemiWeather();
+    final service = WeatherService(client: mock);
+    final param = WeatherParameterModel(area: 'tokyo', date: DateTime.now());
+
+    when(mock.fetchWeather(any)).thenThrow(YumemiWeatherError.unknown);
+    expect(
+      () => service.fetch(param),
+      throwsA(const TypeMatcher<WeatherUnknownException>()),
+    );
   });
 }
