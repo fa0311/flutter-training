2380c1568776b57ebb59073fed616512aabcff56
ふぁ <yuki@yuki0311.com>
2024-08-20 18:05:47 +0900

    feat: widget test を追加する

---
+++ test/view/weather_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/domain/weather_service.dart';
+import 'package:flutter_training/model/weather_model.dart';
+import 'package:flutter_training/provider/weather_provider.dart';
+import 'package:flutter_training/view/weather.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateNiceMocks([MockSpec<WeatherService>()])
+import 'weather_test.mocks.dart';
+
+void main() {
+  final mock = MockWeatherService();
+  final response = WeatherResponseModel(
+    weatherCondition: WeatherType.cloudy,
+    maxTemperature: 10,
+    minTemperature: -10,
+    date: DateTime(2020),
+  );
+
+  testWidgets('天気が正しく表示されるかどうか', (tester) async {
+    // デフォルトのサイズが小さすぎて画面からはみ出してしまう
+    // https://stackoverflow.com/questions/53706569/how-to-test-flutter-widgets-on-different-screen-sizes
+    tester.view.devicePixelRatio = 1;
+    tester.view.physicalSize = const Size(2400, 1600);
+
+    when(mock.fetch(any)).thenReturn(response);
+
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          weatherServiceProvider.overrideWith((_) => mock),
+        ],
+        child: const MaterialApp(
+          home: WeatherScreen(),
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Reload'));
+    await tester.pumpAndSettle();
+
+    // SVG が表示されていることを確認する
+    expect(find.byType(SvgPicture), findsOneWidget);
+
+    // 最高気温が表示されることを確認
+    expect(find.text('10 ℃'), findsOneWidget);
+
+    // 最低気温が表示されることを確認
+    expect(find.text('-10 ℃'), findsOneWidget);
+  });
+}
